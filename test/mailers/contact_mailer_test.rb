ccd432b74113785b2b244d09d5798b95a8ee79cb
Piers Wareham <piers148@hotmail.co.uk>
2016-12-08 10:15:44 +0000

    Mailer complete

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -4,4 +4,11 @@ class ContactMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should return contact email" do
+    mail = ContactMailer.contact_email("pw00161@surrey.ac.uk", "Piers Wareham", "+447944467130", @message = "Problem?")
+    
+    assert_equal ['info@mynotes.com'], mail.to
+    assert_equal ['info@mynotes.com'], mail.from
+  end
 end
