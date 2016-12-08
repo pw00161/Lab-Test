9568f53105ef7d1858f48780c6b22c32e6bb0abe
Piers Wareham <piers148@hotmail.co.uk>
2016-12-08 06:17:32 +0000

    added testing for home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,9 @@ class HomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'h1', 'Car Parking Space Bookings'
+    assert_select 'p1', 'Welcome to the site which manages your car parking bookings!'
+    assert_select 'p2', 'We aim to make sure that you will be able to find a car parking space and how much it will cost you for that space'
   end
 
 end
