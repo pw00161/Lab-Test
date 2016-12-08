02fd132c99b6063ebc34e8b595479e6549a2cd66
Piers Wareham <piers148@hotmail.co.uk>
2016-12-08 07:12:23 +0000

    added new contact us form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#home'
+  get 'contact', to: 'home#contact'
+  post 'request_contact', to: 'home#request_contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -8,5 +8,30 @@ class HomeControllerTest < ActionController::TestCase
     assert_select 'p1', 'Welcome to the site which manages your car parking bookings!'
     assert_select 'p2', 'We aim to make sure that you will be able to find a car parking space and how much it will cost you for that space'
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response:success
+    assert_select'h1', 'Contact Us'
+    assert_select'pc', 'Please complete the following form to get in contact with us.'
+  end
+
+  test "should post request contact but no email" do
+    post :request_contact
+    
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post :request_contact,
+      name: "Piers", email: "pw00161@surrey.ac.uk",
+      telephone: "+447944467130", message: "Problem?"
+      
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
 
 end
